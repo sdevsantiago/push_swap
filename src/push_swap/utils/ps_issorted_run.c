530065d7112ba5b66d5680c18841a322771eb396
sdevsantiago <147100454+sdevsantiago@users.noreply.github.com>
2025-03-07 17:23:14 +0100

    fix: fixed SIGFAULT caused by direct access to node

--- src/push_swap/utils/ps_issorted_run.c
+++ src/push_swap/utils/ps_issorted_run.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 19:09:16 by sede-san          #+#    #+#             */
-/*   Updated: 2025/02/21 19:29:27 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/03/07 13:25:14 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,18 @@ int	ps_issorted_run(t_cdlist **run_start, size_t run)
 	while (current && ps_data(current)->run == run)
 	{
 		if (ps_data(current->previous)->num > ps_data(current)->num)
+		{
 			if (order == ORDER_ASCENDING)
-				order == ORDER_DESCENDING;
+				order = ORDER_DESCENDING;
+		}
 		else
+		{
 			if (order == ORDER_DESCENDING)
 				return (ORDER_MIXED);
+		}
 		current = current->next;
 		if (current == *run_start)
 			break ;
 	}
 	return (order);
-
 }
