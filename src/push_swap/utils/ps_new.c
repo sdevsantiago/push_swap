1200596436f17bfd7604fec49c913f8f4f57265a
sdevsantiago <147100454+sdevsantiago@users.noreply.github.com>
2025-03-13 14:39:51 +0100

    update: replaced malloc for ft_calloc, all values will now be 0 by default

--- src/push_swap/utils/ps_new.c
+++ src/push_swap/utils/ps_new.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 01:26:02 by sede-san          #+#    #+#             */
-/*   Updated: 2025/02/24 01:27:16 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/03/12 21:25:26 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,10 @@ t_push_swap	*ps_new(int num, size_t index)
 {
 	t_push_swap	*new_node;
 
-	new_node = (t_push_swap *)malloc(sizeof(t_push_swap));
+	new_node = (t_push_swap *)ft_calloc(1, sizeof(t_push_swap));
 	if (!new_node)
 		return (NULL);
 	new_node->num = num;
 	new_node->index = index;
-	new_node->run = 0;	//? Updated only in ps_timsort
-	new_node->index_run = 0;
-	new_node->moves_a = 0;
-	new_node->moves_b = 0;
 	return (new_node);
 }
